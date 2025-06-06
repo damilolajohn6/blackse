becd69ee2ae865168ef049f55a40b893b6b01ce5
Damilola john <97457114+damilolajohn6@users.noreply.github.com>
2025-06-06 02:24:05 +0100

    fixing

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "@/styles/styles";
-import ShopInfo from "@/components/Shop/ShopInfo";
+// import ShopInfo from "@/components/Shop/ShopInfo";
 import ShopProfileData from "@/components/Shop/ShopProfileData";
 
 const ShopHomepage = () => {
@@ -8,9 +8,9 @@ const ShopHomepage = () => {
     <div>
       <div className={`${styles.section} bg-[#f5f5f5]`}>
         <div className="w-full flex py-10 justify-between">
-          <div className="w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
+          {/* <div className="w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
             <ShopInfo />
-          </div>
+          </div> */}
           <div className="w-[72%] rounded-[4px]">
             <ShopProfileData isOwner={true} />
           </div>
