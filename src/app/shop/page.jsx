d0b060e14291f5945316920d85f7f55b5517e62a
Damilola john <97457114+damilolajohn6@users.noreply.github.com>
2025-06-06 02:28:04 +0100

    up

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "@/styles/styles";
 // import ShopInfo from "@/components/Shop/ShopInfo";
-import ShopProfileData from "@/components/Shop/ShopProfileData";
+// import ShopProfileData from "@/components/Shop/ShopProfileData";
 
 const ShopHomepage = () => {
   return (
@@ -11,9 +11,9 @@ const ShopHomepage = () => {
           {/* <div className="w-[25%] bg-[#fff] rounded-[4px] shadow-sm overflow-y-scroll h-[90vh] sticky top-10 left-0 z-10">
             <ShopInfo />
           </div> */}
-          <div className="w-[72%] rounded-[4px]">
+          {/* <div className="w-[72%] rounded-[4px]">
             <ShopProfileData isOwner={true} />
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
