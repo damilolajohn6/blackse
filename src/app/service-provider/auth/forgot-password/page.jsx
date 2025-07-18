d9c4415cb167fb104157d639c3e464c672599e39
Damilola john <97457114+damilolajohn6@users.noreply.github.com>
2025-07-18 18:34:17 +0100

    next-router

--- src/app/service-provider/auth/forgot-password/page.jsx
+++ src/app/service-provider/auth/forgot-password/page.jsx
@@ -1,5 +1,6 @@
+"use client";
 import { useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { Loader, Mail, ArrowLeft } from "lucide-react";
 import { toast } from "react-toastify";
